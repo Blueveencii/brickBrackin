cbafff41c1047cf6054006c6d16b2e72a94d7045
Will <wilsonchisessu@gmail.com>
2018-10-31 19:16:32 +0200

    Hallo Vivek

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 //Wrote by Wilson.
-
-//In this Main class, l created the Jframe
+//The goal is to create a brick breaking game.
+//In this Main class, l created the Jframe, outer window, created as an object.
+//Set properties of the Jframe and import the jframe package.
+//pro-obj.setBounds - to create a 10,10,700,600
+//pro-obj.setTitle - Name game
+//pro-obj.setRes - etc.
 public class Main {
     public static void main(String[] args) {
 
@@ -17,6 +21,9 @@ public class Main {
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gamePlay);
 
-    }
+//Class gameplay is added as an object in the jframe object class
+        }
+
+
 }
 
